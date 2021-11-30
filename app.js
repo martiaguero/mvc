9cf9202ea478008eb3078792b664791ace75382c
Martu <martiague97@hotmail.com>
2021-11-30 15:15:22 -0300

    mvc

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = process.env.PORT || 3000;
+const rutasAbout = require('./routes/about')
+const rutasMain = require('./routes/main')
+
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando en http://localhost:${port}`);
+});
+
+app.use(express.static('public'));
+
+app.use('/', rutasAbout);
+app.use('/', rutasMain);
